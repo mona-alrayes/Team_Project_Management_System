c26db610b5fe110a4d553479e90332caf45fe163
mona <mona-alrayes>
2024-09-17 17:28:21 +0300

    'update documentation of project service'

--- app/Services/ProjectService.php
+++ app/Services/ProjectService.php
@@ -7,7 +7,6 @@ use App\Models\User;
 use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 /**
